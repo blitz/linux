3b748638d565698156ff14cc78c2409fbb0d04b0
Zhenyu Wang <zhenyuw@linux.intel.com>
2020-09-14 11:03:02 +0800

    drm/i915/gvt: Fix port number for BDW on EDID region setup
    Current BDW virtual display port is initialized as PORT_B, so need
    to use same port for VFIO EDID region, otherwise invalid EDID blob
    pointer is assigned which caused kernel null pointer reference. We
    might evaluate actual display hotplug for BDW to make this function
    work as expected, anyway this is always required to be fixed first.
    
    Reported-by: Alejandro Sior <aho@sior.be>
    Cc: Alejandro Sior <aho@sior.be>
    Fixes: 0178f4ce3c3b ("drm/i915/gvt: Enable vfio edid for all GVT supported platform")
    Signed-off-by: Zhenyu Wang <zhenyuw@linux.intel.com>
    [Applied from intel-gvt mailing list with conflict resolution]
    Signed-off-by: Julian Stecklina <julian.stecklina@cyberus-technology.de>

--- drivers/gpu/drm/i915/gvt/vgpu.c
+++ drivers/gpu/drm/i915/gvt/vgpu.c
@@ -365,6 +365,7 @@ void intel_gvt_destroy_idle_vgpu(struct intel_vgpu *vgpu)
 static struct intel_vgpu *__intel_gvt_create_vgpu(struct intel_gvt *gvt,
 		struct intel_vgpu_creation_params *param)
 {
+	struct drm_i915_private *dev_priv = gvt->dev_priv;
 	struct intel_vgpu *vgpu;
 	int ret;
 
@@ -432,8 +433,9 @@ static struct intel_vgpu *__intel_gvt_create_vgpu(struct intel_gvt *gvt,
 	if (ret)
 		goto out_clean_sched_policy;
 
-	/*TODO: add more platforms support */
-	if (IS_SKYLAKE(gvt->dev_priv) || IS_KABYLAKE(gvt->dev_priv))
+	if (IS_BROADWELL(dev_priv))
+		ret = intel_gvt_hypervisor_set_edid(vgpu, PORT_B);
+	else
 		ret = intel_gvt_hypervisor_set_edid(vgpu, PORT_D);
 	if (ret)
 		goto out_clean_sched_policy;
